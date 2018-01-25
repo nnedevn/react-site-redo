2b83f71dcfc01da7d6ceac6dbcbd8409ac607c2c
Nick_edu <nick_edu@nameless.local>
2018-01-25 15:47:33 -0800

    Stubbs out Item Component

---
+++ src/data.js
@@ -0,0 +1,17 @@
+const myData = 
+[
+  {
+    productName:'Shingles',
+    imgUrl:'https://dl52d2tabxpgo.cloudfront.net/002/622/131/e.jpg',
+    timerValue:15,
+    price: .5,
+    username: "someSmuck"
+  },
+  {
+    productName:'U.S. Traveler Bradford 5-Piece Luggage',
+    imgUrl:'https://dl52d2tabxpgo.cloudfront.net/001/928/311/e.jpg',
+    timerValue: 10,
+    price: .5,
+    username: "someSmuck"
+  }
+]
--- src/layout/Item.js
+++ src/layout/Item.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 
 class Item extends Component {
+    constructor(props){
+    super();
+    this.state = {
+      timer:null,
+      price: null,
+
+    }
+  }
+
+  updateComponent = (e) => {
+    console.log('reset timer');
+    console.log('update price');
+  } 
+  
   render(){
     return (
+      <div>
+        <div>
+        <a href="#"><h5>{this.props.item.productName}</h5></a>
+      </div>
+      <div className="auction-item-img">
+        <a href=""><img src="{this.props.item.imgUrl}" alt=""/></a>
+        <h2>{timer}</h2>
+        <h5>{user}</h5>
+      </div>
+      <input type="button" onClick={this.updateComponent}/>
+      </div>
     
     )
   }

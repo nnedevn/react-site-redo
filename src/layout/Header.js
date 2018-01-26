5799715feca5262d4480be009082ba76bfe2deb6
rallysport206 <phan.tony125@gmail.com>
2018-01-25 21:27:17 -0800

    small changes

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -15,13 +15,14 @@ class Header extends Component {
           <div className="video-box">
             <div className="img-video">
               <img src="https://dcfv5z6t7wfeu.cloudfront.net//img/landing/march/overstock-image.jpg" alt="overstock" />
-              <div className="video">
+              <div className="img-video">
                 <video width="368" height="208" autoplay="" controls="" poster="https://dcfv5z6t7wfeu.cloudfront.net/img/landing/landing_video_3.jpg" />
               		<source src="https://drxdv8f27mzoi.cloudfront.net/marketing/lander_video_oct.mp4?1" type="video/mp4" />
                 <div className="img-video">
                   <img src="https://dcfv5z6t7wfeu.cloudfront.net//img/landing/march/wholesale-image.jpg" alt="wholesale"/>
                   <div className="full">
                     <img src="https://dcfv5z6t7wfeu.cloudfront.net//img/landing/bbb-2013.png" alt="bbb" class="bbb-image" />
+                    </div>
                     <div className="full">
                       <img src="https://dcfv5z6t7wfeu.cloudfront.net//img/landing/march/quibids-versus.jpg" alt="QuiBids Versus" class="hide-md"/>
                       <div className="full">
@@ -30,7 +31,6 @@ class Header extends Component {
                           <img src="https://dcfv5z6t7wfeu.cloudfront.net//img/landing/march/past-auctions-001.jpg" alt="Past Auctions5"/>
                           <div className="full">
                             <img src="https://dcfv5z6t7wfeu.cloudfront.net//images/landing/43/scrolldownbar.jpg" />
-                          </div>
                         </div>
                       </div>
                     </div>
